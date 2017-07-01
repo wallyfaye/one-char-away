5ddef427507026912af5a55179fe841eb235bcb1
Wally Faye <wally.faye@gmail.com>
2017-07-01 09:58:21 -0400

    setting up simple test

--- OneCharAway.java
+++ OneCharAway.java
@@ -1,10 +1,28 @@
 public class OneCharAway{
+
 	public boolean res = true;
+	private int[] char_val_array = new int[512];
+
 	public OneCharAway(String arg_1, String arg_2){
 		int length_diff = arg_1.length() - arg_2.length();
-		if(length_diff > 1 || length_diff < -1){
+
+		if(length_diff == 1){
+			res = this.testStrings(arg_2, arg_1);
+		} else if (length_diff == -1){
+			res = this.testStrings(arg_2, arg_1);
+		} else if (length_diff == 0){
+			System.out.println(0);
+		} else {
 			res = false;
 		}
+
+	}
+
+	private boolean testStrings(String short_str, String long_str){
+		System.out.println(short_str);
+		System.out.println(long_str);
+		
+		return false;
 	}
 
 }
