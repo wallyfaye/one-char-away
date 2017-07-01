8b68667692fb02b876a271ba6266fc1249d96ef5
Wally Faye <wally.faye@gmail.com>
2017-06-30 21:20:34 -0400

    check string lengths

--- OneCharAway.java
+++ OneCharAway.java
@@ -1,6 +1,10 @@
 public class OneCharAway{
-	public boolean res = false;
+	public boolean res = true;
 	public OneCharAway(String arg_1, String arg_2){
+		int length_diff = arg_1.length() - arg_2.length();
+		if(length_diff > 1 || length_diff < -1){
+			res = false;
+		}
 	}
 
 }
